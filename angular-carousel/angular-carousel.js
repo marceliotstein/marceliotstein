d08d42690ecc5c67abfabe757008251b4262bd8d
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2015-02-18 16:04:35 -0500

    get carousel working with my images

--- angular-carousel/angular-carousel.js
+++ angular-carousel/angular-carousel.js
@@ -72,7 +72,7 @@ angular.module('angular-carousel')
 angular.module('angular-carousel').run(['$templateCache', function($templateCache) {
   $templateCache.put('carousel-indicators.html',
       '<div class="rn-carousel-indicator">\n' +
-        '<span ng-repeat="slide in slides" ng-class="{active: $index==index}" ng-click="goToSlide($index)">●</span>' +
+        '<span ng-repeat="slide in slides" ng-class="{active: $index==index}" ng-click="goToSlide($index)">o</span>' +
       '</div>'
   );
 }]);
