1ab4f03f6654f5c91617f2633d10c600e28dacdf
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2015-02-22 10:36:27 -0500

    replaced index.html with index.php with debug parameter

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+  /*
+   * Angular implementation of MarcEliotStein.com
+   *
+   * optional d=* parameter for debug
+   *
+   */
+<?php
+  $debug = false;
+  if (!empty($_GET['d'])) {
+    $debug = true;
+  }
+?>
 <!DOCTYPE html>
 <html ng-app="projectStore">
 <head>
@@ -10,44 +23,28 @@
   <script type="text/javascript" src="angular-carousel/angular-carousel.js"></script>
   <script type="text/javascript" src="marceliotstein.js"></script>
 </head>
-<body class="mes-body container"
+<body class="mes-body container-fluid"
 
 <div class="row">
-  <div class="testie col-xs-12 hidden-sm hidden-md hidden-lg hidden-xl">
-    XS
-  </div>
-  <div class="testie hidden-xs col-sm-12 hidden-md hidden-lg hidden-xl">
-    SM
-  </div>
-  <div class="testie hidden-xs hidden-sm col-md-12 hidden-lg hidden-xl">
-    MD
-  </div>
-  <div class="testie hidden-xs hidden-sm hidden-md col-lg-12 hidden-xl">
-    LG
-  </div>
-  <div class="testie hidden-xs hidden-sm hidden-md hidden-lg col-xl-12">
-    XL
-  </div>
   <!-- OPTIONAL BUFFER -->
   <div class="hidden-xs hidden-sm col-md-1">
   </div>
 
   <!-- MAIN THEATRE -->
-  <div class="col-xs-12 col-sm-12 col-md-10 text-center">
+  <div class="mes-main-theatre col-xs-12 col-sm-12 col-md-10 text-center">
 
     <!-- HEADER -->
     <div class="mes-title center-block">
-      <!--<img src="images/mesboard800.png" class="img-responsive" alt="Marc Eliot Stein. Advanced website development. Specialty in Drupal, Angular, Bootstrap.">-->
       <img src="images/meslogo1200.png" class="img-responsive" alt="Marc Eliot Stein. Advanced website development. Specialty in Drupal, Angular, Bootstrap.">
       <div class="mes-hr"></div>
     </div>
 
     <!-- MAIN CONTENT -->
-    <div class="col-xs-12 list-group center-block">
+    <div class="ng-cloak col-xs-12 list-group center-block">
       <div ng-controller="DemoCtrl">
 
-        <!-- THE NARROW WAY -->
-        <div class="col-xs-12 col-sm-12 hidden-md hidden-lg hidden-xl">
+        <!-- NARROW XS -->
+        <div class="col-xs-12 hidden-sm hidden-md hidden-lg hidden-xl">
           <div class="carousel-demo">
             <ul rn-carousel mes-carousel-xs rn-carousel-index="carouselIndex2" rn-carousel-auto-slide rn-carousel-pause-on-hover rn-carousel-buffered class="carousel2">
               <li ng-repeat="slide in slides2 track by slide.id" ng-class="'id-' + slide.id">
@@ -68,14 +65,39 @@
           </div>
           <div rn-carousel-indicators ng-if="slides2.length > 1" slides="slides2" rn-carousel-index="carouselIndex2"></div>
 
-          <div class="col-xs-12 col-sm-12 hidden-md hidden-lg hidden-xl">
-            <div class="mes-narrow-blurb center-block">
-              <img src="images/mesblurb600.png" class="img-responsive" alt="xyz" />
-            </div>
+          <div class="col-xs-12 center-block">
+            <img src="images/mesblurb600.png" class="img-responsive" alt="Advanced web development. Social + Responsive" />
           </div>
         </div>
 
-        <!-- THE WIDE WAY -->
+        <!-- NARROW SM -->
+        <div class="hidden-xs col-sm-12 hidden-md hidden-lg hidden-xl">
+          <div class="carousel-demo">
+            <ul rn-carousel mes-carousel-sm rn-carousel-index="carouselIndex2" rn-carousel-auto-slide rn-carousel-pause-on-hover rn-carousel-buffered class="carousel2">
+              <li ng-repeat="slide in slides2 track by slide.id" ng-class="'id-' + slide.id">
+
+                <div class="mes-project-box col-xs-12">
+                  <img class="mes-project-img img-responsive" ng-src={{slide.imgmedium}} />
+
+                  <table class="mes-table">
+                    <tr><td class="mes-narrow-td">
+                      <p class="mes-project-title">{{slide.label}}</p>
+                      <p>{{slide.blurb}} {{slide.tech}}. {{slide.year}}.</p>
+                      <p><a class="mes-anchor" ng-href="{{slide.projlink}}">{{slide.projlink}}</a></p>
+                    </td></tr>
+                  </table>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div rn-carousel-indicators ng-if="slides2.length > 1" slides="slides2" rn-carousel-index="carouselIndex2"></div>
+
+          <div class="mes-narrow-blurb col-sm-12 center-block">
+            <img src="images/mesblurb600.png" alt="Advanced web development. Social + Responsive" />
+          </div>
+        </div>
+
+        <!-- WIDE -->
         <div class="hidden-xs hidden-sm col-md-12 col-lg-12 col-xl-12">
           <table class="mes-table">
             <tr>
@@ -112,10 +134,13 @@
     </div>
 
     <!-- FOOTER -->
-    <div class="col-xs-12 row mes-footer center-block">
-      <!-- XS AND SM ONLY -->
-      <div class="mes-footer-box col-xs-12 col-sm-12 hidden-md hidden-lg hidden-xl">
-        <img class="mes-selfie img-responsive" src="images/mes2015.jpg" />
+    <div class="ng-cloak col-xs-12 row mes-footer center-block">
+      <!-- NARROW -->
+      <div class="mes-footer-box col-xs-12 hidden-sm hidden-md hidden-lg hidden-xl">
+        <img class="mes-selfie img-responsive" src="images/mesbooks600.jpg" />
+      </div>
+      <div class="mes-footer-box hidden-xs col-sm-12 hidden-md hidden-lg hidden-xl">
+        <img class="mes-selfie" src="images/mesbooks600.jpg" />
       </div>
       <div class="mes-footer-box col-xs-12 col-sm-12 hidden-md hidden-lg hidden-xl text-center">
         <table class="mes-table">
@@ -131,8 +156,13 @@
       <!-- WIDE -->
       <div class="mes-footer-box hidden-xs hidden-sm col-md-12">
         <table class="mes-table">
-          <tr><td class="mes-wide-footer-td">
-            <img class="mes-selfie img-responsive" src="images/mes2015.jpg" />
+          <!-- MD ONLY -->
+          <tr><td class="mes-wide-footer-td col-md-12 hidden-lg hidden-xl">
+            <img class="mes-selfie mes-md-selfie" src="images/mesbooks600.jpg" />
+          </td>
+          <!-- LG AND XL -->
+          <tr><td class="mes-wide-footer-td hidden-md col-lg-12">
+            <img class="mes-selfie" src="images/mesbooks600.jpg" />
           </td>
           <td class="mes-wide-footer-td">
             <p>Contact: <b>marc.eliot.stein@gmail.com</b></p>
@@ -152,6 +182,26 @@
   <div class="hidden-xs hidden-sm col-md-1">
   </div>
 </div>
+
+<?php if ($debug): ?>
+  <div class="showbreaks">
+    <div class="col-xs-12 hidden-sm hidden-md hidden-lg hidden-xl">
+      XS
+    </div>
+    <div class="hidden-xs col-sm-12 hidden-md hidden-lg hidden-xl">
+      SM
+    </div>
+    <div class="hidden-xs hidden-sm col-md-12 hidden-lg hidden-xl">
+      MD
+    </div>
+    <div class="hidden-xs hidden-sm hidden-md col-lg-12 hidden-xl">
+      LG
+    </div>
+    <div class="hidden-xs hidden-sm hidden-md hidden-lg col-xl-12">
+      XL
+    </div>
+  </div>
+<?php endif; ?>
 
 <script>
 angular.bootstrap(document, ['DemoApp']);
